fb82c4c952db389606ac42c87011f6ead195da2d
Wyntered <88013111+Wyntered@users.noreply.github.com>
2023-02-04 22:45:17 +1300

    Added start menu
    Player can now click start for a tutorial.

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public CanvasGroup canvasGroup;
+    public Button startButton;
+    public bool open;
+    // Start is called before the first frame update
+    void Start()
+    {
+        open = false;
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+        startButton.onClick.AddListener(toggleStart);
+    }
+
+    public void toggleStart()
+    {
+        open = !open;
+        if(open)
+        {
+            canvasGroup.alpha = 1;
+            canvasGroup.blocksRaycasts = true;
+        }
+        else
+        {
+            canvasGroup.alpha = 0;
+            canvasGroup.blocksRaycasts = false;
+        }
+    }
+}
