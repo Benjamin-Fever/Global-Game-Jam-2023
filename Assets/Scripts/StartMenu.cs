f69420b29cf8766d4d06ebf4d6960f8b53a08c35
Wyntered <88013111+Wyntered@users.noreply.github.com>
2023-02-05 13:36:13 +1300

    Postcards and menus
    Added 3 postcards, changed positioning on space mail, made start menu appear in front of space mail and made it toggleable by clicking anywhere.

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -9,17 +9,38 @@ public class StartMenu : MonoBehaviour
     public CanvasGroup canvasGroup;
     public Button startButton;
     public bool open;
+    public float cooldownTime;
+    public float coolDownTimer;
     // Start is called before the first frame update
     void Start()
     {
+        cooldownTime = 0.1f;
+        coolDownTimer = cooldownTime;
         open = false;
         canvasGroup.alpha = 0;
         canvasGroup.blocksRaycasts = false;
         startButton.onClick.AddListener(toggleStart);
     }
 
+    private void Update()
+    {
+        if (open && Input.GetMouseButtonDown(0))
+        {
+            toggleStart();
+        }
+
+        //Timer prevents clicking button from reopening start.
+        if(coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+        }
+    }
     public void toggleStart()
     {
+        if(coolDownTimer > 0)
+        {
+            return;
+        }
         open = !open;
         if(open)
         {
@@ -31,5 +52,6 @@ public class StartMenu : MonoBehaviour
             canvasGroup.alpha = 0;
             canvasGroup.blocksRaycasts = false;
         }
+        coolDownTimer = cooldownTime;
     }
 }
